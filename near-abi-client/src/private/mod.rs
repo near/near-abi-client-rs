556cc9ec8064fe7c66d25595956dc0ea9ab983ad
Daniyar Itegulov <ditegulov@gmail.com>
2022-07-22 17:24:19 +1000

    resolve clippy errors

--- near-abi-client/src/private/mod.rs
+++ near-abi-client/src/private/mod.rs
@@ -149,6 +149,6 @@ fn expand_subschema(
     expander: &mut Expander,
     schema: &near_sdk::__private::schemars::schema::Schema,
 ) -> proc_macro2::Ident {
-    let schemafy_schema = schemars_schema_to_schemafy(&schema);
+    let schemafy_schema = schemars_schema_to_schemafy(schema);
     format_ident!("{}", expander.expand_type_from_schema(&schemafy_schema).typ)
 }
