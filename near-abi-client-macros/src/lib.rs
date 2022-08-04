3991b9349ca077e52c8a27b8c21b3bcdc91d7f12
Miraculous Owonubi <omiraculous@gmail.com>
2022-08-04 18:27:37 +0400

    contain macro expansion to avoid type collisions

--- near-abi-client-macros/src/lib.rs
+++ near-abi-client-macros/src/lib.rs
@@ -1,12 +1,40 @@
 use near_abi_client::__private::{generate_abi_client, read_abi};
+use quote::{format_ident, quote};
 use std::path::PathBuf;
+
+fn camel_to_snake(s: &str) -> String {
+    let mut words = vec![];
+    let mut buf = String::new();
+    let mut last_upper = false;
+    for c in s.chars() {
+        if !buf.is_empty() && !last_upper && c.is_uppercase() {
+            words.push(buf);
+            buf = String::new();
+        }
+        last_upper = c.is_uppercase();
+        buf.push(c.to_ascii_lowercase());
+    }
+    words.push(buf);
+    words.join("_")
+}
 
 #[proc_macro]
 pub fn near_abi_client(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let abi_def = syn::parse_macro_input!(tokens as AbiDef);
     let near_abi = read_abi(PathBuf::from(&abi_def.path.value()));
 
-    generate_abi_client(near_abi, abi_def.name).into()
+    let client = generate_abi_client(near_abi, abi_def.name.clone());
+
+    let contract_name = format_ident!("{}", abi_def.name);
+    let mod_name = format_ident!("__{}", camel_to_snake(&abi_def.name.to_string()));
+
+    quote! {
+        pub mod #mod_name {
+            #client
+        }
+        use #mod_name::#contract_name;
+    }
+    .into()
 }
 
 struct AbiDef {
